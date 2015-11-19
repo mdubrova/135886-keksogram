62e90e27350bf3b6d858dbfe2ec1905674833d1f
maria <maria.dubrova@teamaol.com>
2015-11-19 22:52:19 +0200

    updated_function_after_review

--- js/check.js
+++ js/check.js
@@ -1,36 +1,45 @@
 function getMessage(a, b) {
 
-	var a = typeof a;
-	var b = typeof b;
+	var aType = typeof a;
+	var bType = typeof b;
 
-	if (a == "boolean" && a == true) {
-	  return ("Переданное GIF-изображение анимировано и содержит" + b + "кадров" );
-	} else {
+	if (aType == "boolean" && a === true) {
+	  return ("Переданное GIF-изображение анимировано и содержит " + b + " кадров" );
+	} else if (aType == "boolean" && a === false) {
 	  return ('Переданное GIF-изображение не анимировано');
 	}
 
-	if (a == "number") {
-	   var attr = b*4
-	   return ("Переданное SVG-изображение содержит" + a + "объектов и" + attr + "аттрибутов");
+	if (aType == "number") {
+	   var attr = b*4;
+	   return ("Переданное SVG-изображение содержит " + a + " объектов  " + attr + " аттрибутов");
 	  }
 
-    if (a.constractor == Array) {
-	     var arr = a;
-		 var result = arr.reduce(function(sum, current) {
-		 return sum + current;
-		 }, 0);
-		 return ("Количество красных точек во всех строчках изображения:" + result);
+    if (aType == "object" && a.length !== undefined) {
+	     // var arr = a;
+		 // var result = arr.reduce(function(sum, current) {
+		 // return sum + current;
+		 // }, 0);
+           var sum = 0;
+		   for (var i = 0; i < a.length - 1; i++) {
+		   	sum = sum + a[i];
+		   };
+		 return ("Количество красных точек во всех строчках изображения: " + sum);
     }
 
-    if (a.constractor == Array && b.constractor == Array) {
-    	var arr = a;
-    	var arr2 = b;
+    if ((aType == "object" && a.length !== undefined) && (bType  == "object" && b.length !== undefined)) {
+    	
+    	var square  = 0;
+    	for (var i = 0; i < a.length -1; i++) {
+    		square  = sguare + (b[i]*a[i])
+    	}
 
-    	var arr3 = arr.map(function(value, index){ return value * arr2[index] });
-    	var square = arr3.reduce(function(sum, current) {
-	    return sum + current;
-	    }, 0);
-        return ("Общая площадь артефактов сжатия:" + square + "пикселей")
+    	//var arr3 = arr.map(function(value, index){ return value * arr2[index] });
+    	//var square = arr3.reduce(function(sum, current) {
+	    //return sum + current;
+	    //}, 0);
+        return ("Общая площадь артефактов сжатия: " + square + " пикселей")
     }
 }
+
+
 
