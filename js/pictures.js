35ba68811ab774d946c29e27f9e4933fcb4b45b7
Maria Dubrova <maria.dubrova@vidible.tv>
2015-12-01 12:45:40 +0200

    code formatting

--- js/pictures.js
+++ js/pictures.js
@@ -1,39 +1,35 @@
 /* global pictures: true */
 'use strict';
 
-(function(){
- var filters = document.querySelector('.filters');
- filters.classList.add('hidden');
+(function() {
+  var filters = document.querySelector('.filters');
+  filters.classList.add('hidden');
 
- pictures.forEach(function(picture){
-  var nodeElement = getElementFromTemplate(picture);
-  document.querySelector('.pictures').appendChild(nodeElement);
- });
+  pictures.forEach(function(picture) {
+   var nodeElement = getElementFromTemplate(picture);
+   document.querySelector('.pictures').appendChild(nodeElement);
+  });
 
- function getElementFromTemplate(data){
-  var pictureTemplate = document.querySelector('#picture-template');
-  var duplicate = pictureTemplate.content.children[0].cloneNode(true);
+  function getElementFromTemplate(data) {
+   var pictureTemplate = document.querySelector('#picture-template');
+   var duplicate = pictureTemplate.content.children[0].cloneNode(true);
 
-  duplicate.querySelector('.picture-comments').textContent = data.comments;
-  duplicate.querySelector('.picture-likes').textContent = data.likes;
+   duplicate.querySelector('.picture-comments').textContent = data.comments;
+   duplicate.querySelector('.picture-likes').textContent = data.likes;
 
-  var image = new Image(182, 182);
+   var image = new Image(182, 182);
 
-  image.onload = function() {
-   duplicate.replaceChild(image, duplicate.querySelector('img'));
-    };
+   image.onload = function() {
+    duplicate.replaceChild(image, duplicate.querySelector('img'));
+   };
 
-  image.onerror = function() {
+   image.onerror = function() {
     duplicate.classList.add('picture-load-failure');
-      };
+   };
 
-  image.src = data.url;
+   image.src = data.url;
 
- return duplicate;
+   return duplicate;
  }
- 
  filters.classList.remove('hidden');
- 
-
 })();
-	
