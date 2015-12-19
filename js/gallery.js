37a94a050b07ce59dd4d5ea088aa0bc71882bbfa
Maria Dubrova <maria.dubrova@vidible.tv>
2015-12-19 20:17:03 +0200

    added js doc to gallery.js and photo.js

--- js/gallery.js
+++ js/gallery.js
@@ -3,8 +3,8 @@
 (function() {
 
  /**
-  * @param {Object} data
-  * @constractor
+  * конструктор для создания обьекта Gallery 
+  * @constructor
   */
 
   function Gallery() {
@@ -14,18 +14,29 @@
 
   }
 
+  /**
+   * показ галереи
+   */
+
   Gallery.prototype.show = function() {
-    //debugger;
     this.element.classList.remove('invisible');
 
     this._closeButton.addEventListener('click', this._onCloseClick);
   };
+ 
+  /**
+   * скрытие галереи
+   */
 
   Gallery.prototype.hide = function() {
     this.element.classList.add('invisible');
     this._closeButton.removeEventListener('click', this._onCloseClick);
   };
 
+ /**
+  * обработчик клика по крестику(закрыть галерею)
+  * @private
+  */
   Gallery.prototype._onCloseClick = function() {
     this.hide();
   };
--- js/photo.js
+++ js/photo.js
@@ -4,12 +4,17 @@
 
  /**
   * @param {Object} data
-  * @constractor
+  * @constructor
+  * конструктор для создания обьекта Photo
   */
 
   function Photo(data) {
     this._data = data;
   }
+
+  /**
+  * создание элемента photo из шаблона 
+  */
 
   Photo.prototype.render = function() {
     var pictureTemplate = document.querySelector('#picture-template');
@@ -18,11 +23,24 @@
     this.element.querySelector('.picture-comments').textContent = this._data.comments;
     this.element.querySelector('.picture-likes').textContent = this._data.likes;
 
+    /**
+     * @type {Image}
+     */
+
     var image = new Image(182, 182);
+
+    /**
+     * функция которая при загрузке фотографии заполняет элемент картинкой
+     */
 
     image.onload = function() {
       this.element.replaceChild(image, this.element.querySelector('img'));
     }.bind(this);
+
+
+    /**
+     * функция которая в случае ошибки при загрузке фотографии заполняет элемент специальной картинкой
+     */
 
     image.onerror = function() {
       this.element.classList.add('picture-load-failure');
